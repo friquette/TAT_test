5d6f3f4eae0e35829d56ed727566eaffc7e6d03e
Thomas Fricard <thomasfricard@gmail.com>
2023-12-01 11:21:33 +0100

    get latest published file and import it with controlers

--- main.py
+++ main.py
@@ -7,29 +7,46 @@ file_template = "{accessory}_{asset}_{status}_{task}_{version}.{extension}"
 work_template = "{accessory}_{asset}/{task}/work"
 publish_template = "{accessory}_{asset}/{task}/publish"
 
-def import_assets():
-    representation = {
+representation = {
         "accessory": "A",
         "asset": "Toto",
         "status": "publish",
         "task": "mode",
         "extension": "ma"
     }
-    workfile_path = Path(os.path.join(PROJECT_ROOT, publish_template.format(**representation)))
-    latest_version = get_latest_version(workfile_path)
 
-    representation["version"] = latest_version + 1
+def import_assets():
+    published_workfile_path = Path(os.path.join(
+        PROJECT_ROOT,
+        publish_template.format(**representation)
+    ))
+
+    latest_published_file, version = get_latest_published_file_and_version(published_workfile_path)
+    latest_published_filepath = Path(os.path.join(
+        published_workfile_path,
+        latest_published_file
+    ))
+    mode = cmds.file(latest_published_filepath, i=True)
+
+    circle_path = Path(os.path.join(PROJECT_ROOT, "circle.ma"))
+    square_path = Path(os.path.join(PROJECT_ROOT, "square.ma"))
+
+    circle_ctrl = cmds.file(circle_path, i=True)
+    square_ctrl = cmds.file(square_path, i=True)
 
 
-def get_latest_version(path):
+def get_latest_published_file_and_version(path):
+    published_files = {}
     versions = []
     for workfile in os.listdir(path):
         file_name, extension = os.path.splitext(workfile)
         version = file_name.split("_")[-1][1:]
         versions.append(int(version))
+        published_files[int(version)] = workfile
 
     versions.sort()
-    return versions[-1]
+    latest_published_file = published_files[versions[-1]]
 
+    return latest_published_file, versions[-1]
 
 import_assets()
