31ff168dd117c47bccf13cd0a3269950ea13475e
Thomas Fricard <thomasfricard@gmail.com>
2023-12-01 16:01:52 +0100

    add some comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import platform
+
 
 PROJECT_ROOT = Path("G:\WORK\TAT_test\project")
 file_template = "{accessory}_{asset}_{status}_{task}_{version}.{extension}"
@@ -37,42 +37,42 @@ def import_assets():
     ))
     mode = cmds.file(latest_published_filepath, i=True)
 
-    circle_path = Path(os.path.join(PROJECT_ROOT, "circle.ma"))
-    square_path = Path(os.path.join(PROJECT_ROOT, "square.ma"))
-
-    circle_ctrl = cmds.file(circle_path, i=True)
-    square_ctrl = cmds.file(square_path, i=True)
-
 
 def build_speed_rig():
+    # rename the mesh
     mesh = cmds.ls(exactType="mesh", long=True)[0]
     mesh = mesh.split('|')[1]
     new_name_mesh = f"{mode_representation['asset']}_msh"
     cmds.rename(mesh, new_name_mesh)
 
+    # import the controlers
     circle_path = os.path.join(PROJECT_ROOT, "circle.ma")
     square_path = os.path.join(PROJECT_ROOT, "square.ma")
     circle_ctrl_01 = cmds.file(circle_path, i=True)
     circle_ctrl_02 = cmds.file(circle_path, i=True)
     square_ctrl = cmds.file(square_path, i=True)
 
+    # rename the controlers
     controlers = cmds.ls(type="nurbsCurve", long=True)
     controlers_name = ["world_ctrl", "fly_ctrl", "global_ctrl"]
     for controler, name in zip(controlers, controlers_name):
         controler = controler.split('|')[1]
         cmds.rename(controler, name)
 
+    # apply freeze transform and center pivot
     objects = controlers_name + [new_name_mesh]
     for object in objects:
         cmds.select(object)
         cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
         cmds.xform(cp=True)
 
+    # create the groups and the parenting
     root_group = cmds.group(name="ROOT", empty=True)
     geo_group = cmds.group(new_name_mesh, name='geo', parent=root_group)
     cmds.group('world_ctrl', name='ctrl')
     cmds.parent('global_ctrl', 'world_ctrl')
 
+    # set the size of the box to the size of the mesh
     bbox = cmds.exactWorldBoundingBox(new_name_mesh)
     width = bbox[3] - bbox[0]
     height = bbox[4] - bbox[1]
@@ -80,11 +80,11 @@ def build_speed_rig():
     cmds.scale(width, height, depth, 'fly_ctrl')
     cmds.parent('fly_ctrl', 'global_ctrl')
 
+    # create the joint and the skin
     cmds.select(clear=True)
     root_joint = cmds.joint(name='root')
     skin_cluster = cmds.skinCluster(root_joint, new_name_mesh, toSelectedBones=True, bindMethod=0, skinMethod=0, normalizeWeights=1)[0]
     cmds.skinPercent(skin_cluster, new_name_mesh, transformValue=[root_joint, 1.0])
-
     cmds.select(clear=True)
     cmds.group(root_joint, name='jnt')
     cmds.parent('jnt', 'ctrl')
