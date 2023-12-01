2bc19e7a32a2b32200658f4cefc7764188b9e5c1
Thomas Fricard <thomasfricard@gmail.com>
2023-12-01 12:32:27 +0100

    import and rename controlers

--- main.py
+++ main.py
@@ -35,6 +35,25 @@ def import_assets():
     square_ctrl = cmds.file(square_path, i=True)
 
 
+def build_speed_rig():
+    mesh = cmds.ls(exactType="mesh", long=True)[0]
+    mesh = mesh.split('|')[1]
+    new_name_mesh = f"{representation['asset']}_msh"
+    cmds.rename(mesh, new_name_mesh)
+
+    circle_path = os.path.join(PROJECT_ROOT, "circle.ma")
+    square_path = os.path.join(PROJECT_ROOT, "square.ma")
+    circle_ctrl = cmds.file(circle_path, i=True)
+    circle_ctrl = cmds.file(circle_path, i=True)
+    square_ctrl = cmds.file(square_path, i=True)
+
+    controlers = cmds.ls(type="nurbsCurve", long=True)
+    controlers_name = ["world_ctrl", "fly_ctrl", "global_ctrl"]
+    for controler, name in zip(controlers, controlers_name):
+        controler = controler.split('|')[1]
+        cmds.rename(controler, name)
+
+
 def get_latest_published_file_and_version(path):
     published_files = {}
     versions = []
@@ -49,4 +68,6 @@ def get_latest_published_file_and_version(path):
 
     return latest_published_file, versions[-1]
 
+
 import_assets()
+build_speed_rig()
